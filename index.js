d63c92c41f76e814a34cc9b96bc28c9f836f9c1d
gabrieltrudes <esquecimeunome00@hotmail.com>
2024-10-15 02:54:55 -0300

    Comentários e identação do código

--- index.js
+++ index.js
@@ -1,87 +1,95 @@
-require('dotenv/config');
-const { Client } = require('discord.js');
-const { OpenAI } = require('openai');
+require("dotenv/config");
+const { Client } = require("discord.js");
+const { OpenAI } = require("openai");
 
 const client = new Client({
-    intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent']
+  intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"],
 });
 
-client.on('ready', () => {
-    console.log('Bot online.');
+client.on("ready", () => {
+  console.log("Bot online.");
 });
 
-const IGNORE_PREFIX = "!";
-const CHANNELS = ['1295606665659420733']
+const IGNORE_PREFIX = "!"; // Linha opcional, somente irá fazer o bot ignorar mensagens com o prefixo "!". Assim o bot irá ignorar caso realize algum comando que não seja de responsabilidade do mesmo.
+const CHANNELS = ["Insira o ID do canal do Discord."];
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_KEY,
-})
+  apiKey: process.env.OPENAI_KEY,
+});
 
-client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
-    if (message.content.startsWith(IGNORE_PREFIX)) return;
-    if (!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) return;
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(IGNORE_PREFIX)) return;
+  if (
+    !CHANNELS.includes(message.channelId) &&
+    !message.mentions.users.has(client.user.id)
+  )
+    return;
 
-    await message.channel.sendTyping();
+  await message.channel.sendTyping();
 
-    const sendTypingInterval = setInterval(() => {
-        message.channel.sendTyping();
-    }, 5000)
+  const sendTypingInterval = setInterval(() => {
+    message.channel.sendTyping();
+  }, 5000);
 
-    let conversation = [];
+  let conversation = [];
+  conversation.push({
+    role: "system",
+    content: "O bot Ancião é um chatbot amigável.",
+  });
+
+  let prevMessages = await message.channel.messages.fetch({ limit: 10 });
+  prevMessages.reverse();
+
+  prevMessages.forEach((msg) => {
+    if (msg.author.bot && msg.author.id !== client.user.id) return;
+    if (msg.content.startsWith(IGNORE_PREFIX)) return;
+
+    const username = msg.author.username
+      .replace(/\s+/g, "_")
+      .replace(/[^\w\s]/gi, "");
+
+    if (msg.author.id === client.user.id) {
+      conversation.push({
+        role: "assistant",
+        name: username,
+        content: msg.content,
+      });
+
+      return;
+    }
+
     conversation.push({
-        role: 'system',
-        content: 'O bot Ancião é um chatbot amigável.'
+      role: "user",
+      name: username,
+      content: msg.content,
     });
+  });
 
-    let prevMessages = await message.channel.messages.fetch({limit: 10});
-    prevMessages.reverse();
-
-    prevMessages.forEach((msg) => {
-        if (msg.author.bot && msg.author.id !== client.user.id) return; 
-        if (msg.content.startsWith(IGNORE_PREFIX)) return;
-
-        const username = msg.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, '');
-
-        if (msg.author.id === client.user.id) {
-            conversation.push({
-                role: 'assistant',
-                name: username,
-                content: msg.content,
-            });
-
-            return;
-        }
-
-        conversation.push({
-            role: 'user',
-            name: username,
-            content: msg.content,
-        });
+  const response = await openai.chat.completions
+    .create({
+      model: "gpt-3.5-turbo",
+      messages: conversation,
     })
+    .catch((error) => console.error("Ancião Bot Error:\n", error));
 
-    const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        messages: conversation,
-    })
-    .catch((error) => console.error('Ancião Bot Error:\n', error));
+  clearInterval(sendTypingInterval);
 
-    clearInterval(sendTypingInterval);
+  if (!response) {
+    message.reply(
+      "Estou passando por alguns problemas com a OpenAI API. Tente novamente em alguns minutos."
+    );
+    return;
+  }
 
-    if(!response) {
-        message.reply("Estou passando por alguns problemas com a OpenAI API. Tente novamente em alguns minutos.")
-        return;
-    }
+  const responseMessage = response.choices[0].message.content;
+  const chunkSizeLimit = 2000;
 
-    const responseMessage = response.choices[0].message.content;
-    const chunkSizeLimit = 2000;
+  for (let i = 0; i < responseMessage.length; i += chunkSizeLimit) {
+    const chunk = responseMessage.substring(i, i + chunkSizeLimit);
 
-    for (let i = 0; i < responseMessage.length; i+= chunkSizeLimit) {
-        const chunk = responseMessage.substring(i, i + chunkSizeLimit);
-
-        await message.reply(chunk);
-    }
+    await message.reply(chunk);
+  }
 });
 
 client.login(process.env.TOKEN);
-
